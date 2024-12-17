68704a06038b95984dd3d7d3eabc2566bee3db8f
LeeAnh836 <leeanh836@gmail.com>
2024-12-18 06:35:13 +0700

    sửa lại phần kết nối với mysql trên docker

--- backend/models/database.py
+++ backend/models/database.py
@@ -15,14 +15,15 @@ DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD", "")
 DB_NAME = os.getenv("DB_NAME")
 
-
+# Kiểm tra các biến môi trường bị thiếu
 missing_vars = [var for var in ["DB_HOST", "DB_PORT", "DB_USER", "DB_PASSWORD", "DB_NAME"] if not os.getenv(var)]
 if missing_vars:
     raise ValueError(f"Các biến môi trường sau không được thiết lập: {', '.join(missing_vars)}")
 
-
+# Tạo URL kết nối MySQL
 SQLALCHEMY_DATABASE_URL = f"mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
+# Kết nối cơ sở dữ liệu
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
