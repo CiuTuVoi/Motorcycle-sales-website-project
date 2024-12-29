8e95f48d03ff73c26c693cc68921e39bd3ed6efc
LeeAnh836 <leeanh836@gmail.com>
2024-12-30 06:57:10 +0700

    xóa file thửa

--- backend/password_utils.py
+++
@@ -1,9 +0,0 @@
-from bcrypt import hashpw, gensalt, checkpw
-
-def hash_password(plain_password):
-    """Mã hóa mật khẩu từ chuỗi gốc."""
-    return hashpw(plain_password.encode('utf-8'), gensalt()).decode('utf-8')
-
-def verify_password(plain_password, hashed_password):
-    """Xác minh mật khẩu người dùng nhập với mật khẩu đã mã hóa."""
-    return checkpw(plain_password.encode('utf-8'), hashed_password.encode('utf-8'))
