2db533b1a60cb619237c1fb5e24ca13c0e177c43
LeeAnh836 <leeanh836@gmail.com>
2025-01-05 20:13:23 +0700

    thêm router hình ảnh

--- backend/main.py
+++ backend/main.py
@@ -8,6 +8,9 @@ from api.loai_xe import router as loaixe_router # Import router từ loai_xe.py
 from api.khuyen_mai import router as khuyenmai_router # Import router từ khuyenmai.py
 from api.thong_so_ky_thuat import router as thongsokythuat_router # Import router từ thong_so_ky_thuat.py
 from api.thong_bao_phan_hoi import router as thongbaophanhoi_router # Import router từ thong_bao_phan_hoi.py
+from api.don_hang import router as donhang_router # Import router từ don_hang.py
+from api.anh_xe import router as anhxa_router # Import router từ anh_xe.py
+
 import sys
 import os
 
@@ -25,3 +28,5 @@ app.include_router(loaixe_router)
 app.include_router(khuyenmai_router)
 app.include_router(thongsokythuat_router)
 app.include_router(thongbaophanhoi_router)
+app.include_router(donhang_router)
+app.include_router(anhxa_router)
