4e7d67f591eb53950cecd34fadea227c678dbab8
LeeAnh836 <leeanh836@gmail.com>
2025-01-16 12:52:54 +0700

    xóa import không cần thiết

--- backend/models/database.py
+++ backend/models/database.py
@@ -2,7 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from dotenv import load_dotenv
-from .models import *
 import os
 
 load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), '..', '.env'))
